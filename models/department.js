cc69d2056ae6eea1d5bd11fc7f08fa9b6b45e8d2
Jack Lythgoe <jacklythgoe@hotmail.com>
2024-07-09 11:02:32 +0100

    Fixed

--- models/department.js
+++ models/department.js
@@ -1,54 +1,22 @@
 module.exports = (sequelize, Sequelize) => {
-<<<<<<< Updated upstream
-  const Department = sequelize.define(
-    "department",
-    {
-      department_id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+  const Department = sequelize.define("department",
+  {
+      department_id:{
+          type: Sequelize.INTEGER,
+          primaryKey:true,
+          field: 'department_id'
       },
       department_name: {
-        type: Sequelize.STRING,
-        allowNull: false,
+          type: Sequelize.STRING
       },
-    },
-    {
+  },
+
+  {
       timestamps: false,
       freezeTableName: true,
-      tableName: 'department',
-    }
+      tableName: 'department'
+  }
   );
-  // Primary key functionality
-  Department.associate = (models) => {
-    Department.hasMany(models.User, {
-      foreignKey: 'department_id',
-      as: 'users',
-    });
-  };
 
   return Department;
-};
-=======
-    const Department = sequelize.define("department",
-    {
-        department_id:{
-            type: Sequelize.INTEGER,
-            primaryKey:true,
-            field: 'department_id'
-        },
-        department_name: {
-            type: Sequelize.STRING
-        },
-    },
-
-    {
-        timestamps: false,
-        freezeTableName: true,
-        tableName: 'department'
-    }
-    );
-
-    return Department;
-};   
->>>>>>> Stashed changes
+}; 
