f9fc669f15dfb50ffe05c59860649a3f7166e634
614987704 <john217168@gmail.com>
2024-08-30 10:03:26 +0100

    http://localhost:8081/phpmyadmin/

--- database.sql
+++ database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2024 at 10:07 AM
+-- Generation Time: Aug 30, 2024 at 11:02 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -83,7 +83,7 @@ INSERT INTO `skill` (`skill_id`, `skill_name`, `skill_category_id`) VALUES
 (2, 'Black Box Testing', 2),
 (3, 'C++', 1),
 (4, 'White Box Testing', 2),
-(5, 'HTML', 1);
+(5, 'HTML', 2);
 
 -- --------------------------------------------------------
 
@@ -102,7 +102,8 @@ CREATE TABLE `skill_category` (
 
 INSERT INTO `skill_category` (`skill_category_id`, `skill_category_name`) VALUES
 (1, 'Programming'),
-(2, 'Testing');
+(2, 'Testing'),
+(4, 'Research');
 
 -- --------------------------------------------------------
 
@@ -126,7 +127,10 @@ CREATE TABLE `skill_enrolment` (
 INSERT INTO `skill_enrolment` (`skill_enrolment_id`, `user_id`, `skill_id`, `skill_strength_id`, `expiry_date`, `notes`) VALUES
 (1, 1, 2, 1, '2024-06-27 14:31:59', 'Beginner Black Box Tester'),
 (2, 2, 4, 1, '2024-06-27 14:32:58', 'Beginner White Box Tester'),
-(3, 1, 3, 2, '2024-06-27 14:33:19', 'Intermediate C++ Programmer');
+(3, 1, 3, 2, '2024-06-27 14:33:19', 'Intermediate C++ Programmer'),
+(4, 1, 1, 1, '2024-07-19 00:00:00', 'Hello World!'),
+(5, 1, 1, 1, '2024-07-19 00:00:00', 'Hello World!'),
+(6, 1, 1, 1, '2024-07-19 00:00:00', 'Initial notes');
 
 -- --------------------------------------------------------
 
@@ -180,7 +184,7 @@ CREATE TABLE `user` (
   `last_name` varchar(50) NOT NULL,
   `department_id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `job_role_id` int(11) NOT NULL,
   `system_role_id` int(11) NOT NULL,
   `date_joined` date DEFAULT NULL
@@ -191,9 +195,15 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `department_id`, `email`, `password`, `job_role_id`, `system_role_id`, `date_joined`) VALUES
-(1, 'John', 'Doe', 1, 'johndoe@staffs.com', 'newsecurepassword', 1, 3, '2024-07-09'),
-(2, 'Zak', 'Wilkinson', 2, 'zakwilkinson@staffs.com', 'password123', 2, 2, '2024-06-20'),
-(3, 'John', 'Dean', 2, 'johndean@staffs.com', 'password123', 3, 3, '2024-07-09');
+(1, 'John', 'Doe', 1, 'johndoe@staffs.com', '$2b$10$k1uOEqXf1Mg4alllj09nBu0EJ.8doXfqH4IhqE6ZDF30kWVnRlMmS', 1, 3, '2024-07-09'),
+(2, 'Zak', 'Wilkinson', 2, 'zakwilkinson@staffs.com', '$2b$10$DMdbJF1pGshD5oRtkWKSYuEeXGdG/BdMlau.zYvPWGotCmE9kPyfO', 2, 2, '2024-06-20'),
+(3, 'John', 'Dean', 2, 'johndean@staffs.com', '$2b$10$K7Wi1tECsOiTMpikHX1nPOESMQ9q2VAY.nnLElxOfz6umUmackBam', 3, 3, '2024-07-09'),
+(17, 'Bob', 'Johnson', 1, 'bob.johnson1721235842040@example.com', '$2b$10$oWvvdAfaSOUbhtdt8aShaOZXMN.stg6ZCR3r6RTaDUJvhe.x1gHI2', 1, 1, '2024-07-17'),
+(111, 'Test', 'User', 1, 'testuser@example.com', '$2b$10$BHaFYSP9ByAjMBlU5pjhke.XIGT2EVg98J2i2IO1xmYVipDN8KjCu', 1, 1, '2024-08-21'),
+(112, 'Test', 'User', 1, 'testuser@example.com', '$2b$10$SARamqVDy35/c3pAWz7PeOF/2BhTPK4kQs8Ir2iaRn23M2dD06uvu', 1, 1, '2024-08-21'),
+(113, 'User', 'ToDelete', 1, 'user.todelete1724266440978@example.com', '$2b$10$GotanAXmUjDylJtJAzFgy.4uhsopW4V5/QcYGNCWxxsWg0eA0g7ke', 1, 1, '2024-08-21'),
+(114, 'User', 'ToDelete', 1, 'user.todelete1724266476209@example.com', '$2b$10$lYnl4MIPVjBU36IDqfXICO2.1fBbEbxcW2Bi4aJ/9zpmou/A6qJG.', 1, 1, '2024-08-21'),
+(119, 'Test', 'User', 1, 'testuser@example.com', '$2b$10$T8EF54CUTVpSABfWPB4sReRurW9pCaP4xdwouie7duybvQ21BgZ/.', 1, 1, '2024-08-22');
 
 --
 -- Indexes for dumped tables
@@ -262,49 +272,49 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `job_role`
 --
 ALTER TABLE `job_role`
-  MODIFY `job_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `job_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `skill`
 --
 ALTER TABLE `skill`
-  MODIFY `skill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `skill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
 
 --
 -- AUTO_INCREMENT for table `skill_category`
 --
 ALTER TABLE `skill_category`
-  MODIFY `skill_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `skill_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
 
 --
 -- AUTO_INCREMENT for table `skill_enrolment`
 --
 ALTER TABLE `skill_enrolment`
-  MODIFY `skill_enrolment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `skill_enrolment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `skill_strength`
 --
 ALTER TABLE `skill_strength`
-  MODIFY `skill_strength_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `skill_strength_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `system_role`
 --
 ALTER TABLE `system_role`
-  MODIFY `system_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `system_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
 
 --
 -- Constraints for dumped tables
