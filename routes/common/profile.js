c98ce4b65adf938c4a2e1845dc40a877ec90d483
Jack Lythgoe <jacklythgoe@hotmail.com>
2024-09-06 09:21:43 +0100

    My Profile page is now editable

--- routes/common/profile.js
+++ routes/common/profile.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../../middleware/authenticateToken');
 const db = require('../../models');
+const bcrypt = require('bcrypt');
 const User = db.user;
 
 router.get('/', authenticateToken, async (req, res) => {
@@ -23,6 +24,34 @@ router.get('/', authenticateToken, async (req, res) => {
         res.render('common/profile', { user });
     } catch (error) {
         console.error('Error fetching profile:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/', authenticateToken, async (req, res) => {
+    const { first_name, last_name, email, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { user_id: req.user.userId } });
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        const updatedData = {
+            first_name: first_name || user.first_name,
+            last_name: last_name || user.last_name,
+            email: email || user.email
+        };
+
+        if (password) {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            updatedData.password = hashedPassword;
+        }
+        await User.update(updatedData, { where: { user_id: user.user_id } });
+
+        res.redirect('/profile');
+    } catch (error) {
+        console.error('Error updating profile:', error);
         res.status(500).send('Internal Server Error');
     }
 });
